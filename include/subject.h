b2b909ffaae15cc1bf2f55e316cc4789eecd46bd
cristalmirror <crisrojari@gmail.com>
2026-08-26 23:31:24 -0300

    add code

--- include/subject.h
+++ include/subject.h
@@ -6,11 +6,18 @@
 #ifndef SUBJECT_H
 #define SUBJECT_H
 
+/*input & output buffer size*/
+#define IN_BUF_SIZE 65536
+#define OUT_BUF_SIZE 65536
+
+
 typedef struct Subject Subject;
 
 struct Subject {
     int val;
+    void (*fatal)(const char *msg);
     void (*read_subject)(Subject *self);
+    void (*save_subject)(Subject *self, char **msg, const char **dir, const char **outpath);
     void (*close_subject)(Subject *self);
 };
 
--- src/subject.c
+++ src/subject.c
@@ -2,13 +2,76 @@
  * this archive have all operaticons needed to create and manipulation of a
  * subject and create the archive .xz . 
  */
-
+#include <lzma.h>
+#include <error.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "../include/subject.h"
+
+static void _fatal(const char *msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+} 
 
 void _read_subject(Subject *self) {
     printf("Valor >> %i\n",self->val);
+}
+/*compres and save al archives and information*/
+void _save_subject(Subject *self, char **msg, const char **dir, const char **outpath) {
+
+    int pipefd[2];
+    if (pipe(pipefd) == -1) self->fatal("pipe");
+
+    pid_t pid = fork();
+    if (pid == -1) self->fatal("fork");
+
+    if (pid == 0) {
+      /*
+       * son process: execute tar -cf - -C <pernt_of_dir> <basename>
+       * redirect stdout of son process to the of write pipe 
+       */
+        close(pipefd[0]);
+        if (dup2(pipefd[1], STDOUT_FILENO) == -1) self->fatal("dup2");
+        close(pipefd[1]);
+
+        // tar execute
+        //we use "tar -cf - -C <parent> <base>" to that the tar don't include absulete route
+        char *dircopy = realpath(dir, NULL);
+        if (!dircopy) self->fatal("realpath");
+
+        char *last_slash = strrchr(dircopy, NULL);
+        char parent[PATH_MAX];
+        char base[PATH_MAX];
+        if (last_slash == NULL) {
+            // no slash, use "." like parent
+            strcpy(parent, ".");
+            strcpy(base,dircopy,PATH_MAX);
+        } else if (last_slash == dircopy) {
+            //path starts with "/" and is like "/foo"
+            strcpy(parent, "/", PATH_MAX);
+            strcpy(parent, last_slash + 1, PATH_MAX);
+        } else {
+
+            size_t p_len = last_slash - dircopy;
+            if (p_len >= PATH_MAX) p_len = PATH_MAX - 1;
+            strcpy(parent, dircopy, p_len);
+            parent[p_len] = '\0';
+            strcpy(base, last_slash + 1, PATH_MAX);
+        }
+        free(dircopy);
+        execlp("tar", "tar", "-cf", "-", "-C", parent, base, (char *)NULL);
+        perror("execlp tar");
+        _exit(127);
+    }
+
+    close(pipefd[1]);
+    
 }
 
 /*destructor*/
@@ -30,7 +93,7 @@ Subject *new_subject(int value) {
     new->val = value;
     new->read_subject = _read_subject;
     new->close_subject = _close_subject;
-
+    new->fatal = _fatal;
     return new;
 }
 
